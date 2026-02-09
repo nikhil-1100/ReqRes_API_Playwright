d8d5456bdd7647d71179f67a0aa686cd7614471a
nikhil-1100 <optimalaces.2001@gmail.com>
2026-02-10 01:52:37 +0530

    ReqRes API Testing

--- playwright.config.js
+++ playwright.config.js
@@ -1,18 +1,40 @@
 import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   testDir: './testAssets/tests',
-  globalSetup: './testAssets/api/globalSetup.js', 
   
+  globalSetup: './testAssets/api/globalSetup.js',
+
+  timeout: 30 * 1000,
+  
+  expect: {
+    timeout: 5000
+  },
+
   reporter: [
     ['list'],
     ['monocart-reporter', {  
-        name: "ReqRes API Report",
+        name: "ReqRes API Test Report",
         outputFile: './testAssets/artifacts/monocart-report/index.html',
+    }],
+    ['html', { 
+        outputFolder: 'testAssets/artifacts/playwright-report', 
+        open: 'never' 
     }]
   ],
+
   use: {
     baseURL: 'https://reqres.in',
+    
+    extraHTTPHeaders: {
+      'x-api-key': (process.env.REQRES_TOKEN || '').trim(),
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+
     trace: 'on',
   },
 });
