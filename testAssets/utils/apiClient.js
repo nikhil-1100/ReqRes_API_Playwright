5a14d57c10f98c12a6204e0336b63b4cfbd1ded4
nikhil-1100 <optimalaces.2001@gmail.com>
2026-02-10 02:04:22 +0530

    ReqRes API Testing

--- testAssets/utils/apiClient.js
+++ testAssets/utils/apiClient.js
@@ -2,11 +2,15 @@ import { request } from '@playwright/test';
 import { BASE_URL, REQRES_TOKEN } from './env.js';
 
 export async function getApiContext() {
+  const cleanToken = (REQRES_TOKEN || '').replace(/[\n\r\s\t]/g, '');
+
   return await request.newContext({
     baseURL: BASE_URL,
     extraHTTPHeaders: {
       'Content-Type': 'application/json',
-      ...(REQRES_TOKEN && { 'x-api-key': REQRES_TOKEN })
+      'Accept': 'application/json',
+      // Only add the header if the token exists
+      ...(cleanToken && { 'x-api-key': cleanToken })
     }
   });
 }
